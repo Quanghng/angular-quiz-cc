fd75ac7e03914daaa25ce8065068004bd682b3af
Quanghng <90704201+Quanghng@users.noreply.github.com>
2025-10-15 10:41:24 +0200

    Add categories

--- src/app/categorie/categorie.component.ts
+++ src/app/categorie/categorie.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categorie',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './categorie.component.scss'
 })
 export class CategorieComponent {
+  categorie: number = 0;
+
+  constructor(private router: Router) { }
+
+  goToAngular() {
+    this.categorie = 1
+    this.router.navigate(['/quiz', this.categorie]);
+  }
+
+  goToJavascirpt() {
+    this.categorie = 2
+    this.router.navigate(['/quiz', this.categorie]);
+  }
+
+  goToHtml() {
+    this.categorie = 3
+    this.router.navigate(['/quiz', this.categorie]);
+  }
+
+  goToCss() {
+    this.categorie = 4
+    this.router.navigate(['/quiz', this.categorie]);
+  }
 
 }
